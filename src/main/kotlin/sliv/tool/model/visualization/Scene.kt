de2a591c6ab84b39a6119c2df83fda450bd00cff
Alexander-Ploskin <ploskin0107@gmail.com>
2022-10-12 23:25:16 +0300

    Scene aggregates layers

--- src/main/kotlin/sliv/tool/model/visualization/Scene.kt
+++ src/main/kotlin/sliv/tool/model/visualization/Scene.kt
@@ -3,7 +3,7 @@ package sliv.tool.model.visualization
 //Scene controls virtualization, so it is not needed to supply it with all frames from the start.
 //View demands new frames from the controller, controller asks this model about it
 //getFrame function implemented in the controller, which knows about .
-class Scene(val getFrame: (Int) -> VisualizationFrame?, val framesCount: Int) {
+class Scene(val getFrame: (Int) -> VisualizationFrame?, val framesCount: Int, val layers: List<Layer>) {
     private val loadedFrames: Map<Int, VisualizationFrame> = HashMap<Int, VisualizationFrame>()
     private val viewportBorders = Pair(Pair(0, 10), Pair(0, 20))
 }
