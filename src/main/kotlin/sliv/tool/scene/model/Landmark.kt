7f9ded3ffdfa94e3422cc176933fa796a3a451eb
Alexander-Ploskin <ploskin0107@gmail.com>
2022-10-16 17:22:05 +0300

    make sealed

--- src/main/kotlin/sliv/tool/scene/model/Landmark.kt
+++ src/main/kotlin/sliv/tool/scene/model/Landmark.kt
@@ -1,3 +1,3 @@
 package sliv.tool.scene.model
 
-abstract class Landmark(val uid: Long, val layer: Layer)
+sealed class Landmark(val uid: Long, val layer: Layer)
--- src/main/kotlin/sliv/tool/scene/model/Layer.kt
+++ src/main/kotlin/sliv/tool/scene/model/Layer.kt
@@ -6,7 +6,7 @@ import javafx.scene.paint.Color
 //Layers properties being edited in the settings menu.
 //Settings menu appearance depends on type of the corresponding layer.
 //Meaningful changes here provokes scene redrawing.
-abstract class Layer(val name: String) {
+sealed class Layer(val name: String) {
     var opacity: Double = DEFAULT_OPACITY
         set(value) {
             if (!(0.0..100.0).contains(value))
