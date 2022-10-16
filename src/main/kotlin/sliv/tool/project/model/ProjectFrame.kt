6704c486b470e322b31bb337c4a1a18606fd214a
Alexander-Ploskin <ploskin0107@gmail.com>
2022-10-16 14:30:42 +0300

    Represent timestamp as long

--- src/main/kotlin/sliv/tool/project/model/ProjectFrame.kt
+++ src/main/kotlin/sliv/tool/project/model/ProjectFrame.kt
@@ -1,6 +1,5 @@
 package sliv.tool.project.model
 
 import java.nio.file.Path
-import java.time.LocalDateTime
 
-data class ProjectFrame(val timestamp: LocalDateTime, val imagePath: Path, val outputs: List<LandmarksFile>)
+data class ProjectFrame(val timestamp: Long, val imagePath: Path, val outputs: List<LandmarksFile>)
