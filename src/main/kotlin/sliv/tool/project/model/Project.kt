d7d317f222b93615aa400b27ae44972bda8aaf97
Alexander-Ploskin <ploskin0107@gmail.com>
2022-10-17 11:11:40 +0300

    Project knows about layers which contains

--- src/main/kotlin/sliv/tool/project/model/Project.kt
+++ src/main/kotlin/sliv/tool/project/model/Project.kt
@@ -2,4 +2,4 @@ package sliv.tool.project.model
 
 import java.nio.file.Path
 
-data class Project(val currentDirectory: Path, val frames: List<ProjectFrame>)
+data class Project(val currentDirectory: Path, val frames: List<ProjectFrame>, val layers: List<ProjectLayer>)
