d1d2ba6974a31ef691fde2fef668343c718d122f
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-10-19 17:15:18 +0300

    added controller

---
+++ src/main/kotlin/sliv/tool/importer/controller/ImporterConroller.kt
@@ -0,0 +1,8 @@
+package sliv.tool.importer.controller
+
+import javafx.beans.property.SimpleStringProperty
+import tornadofx.Controller
+
+class ImporterController : Controller() {
+    val directoryPath = SimpleStringProperty()
+}
