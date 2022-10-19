d75586ed7b0a1c94b27550886e6e3f5d68d24930
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-10-19 17:15:08 +0300

    main view for importer

---
+++ src/main/kotlin/sliv/tool/importer/view/ImporterView.kt
@@ -0,0 +1,17 @@
+package sliv.tool.importer.view
+
+import javafx.geometry.Insets
+import tornadofx.borderpane
+
+import tornadofx.View
+
+
+class ImporterView: View("Choose working directory") {
+    override val root = borderpane {
+        prefWidth = 500.0
+        prefHeight = 600.0
+        padding = Insets(5.0, 0.0, 0.0, 20.0)
+        top<ChoosingDirectoryView>()
+    }
+
+}
