492a75e30bbda822c1b0110dfcb6f0d92bbd1793
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-10-19 17:14:41 +0300

    added choosing directory

---
+++ src/main/kotlin/sliv/tool/importer/view/ChoosingDirectoryView.kt
@@ -0,0 +1,32 @@
+package sliv.tool.importer.view
+
+import javafx.geometry.Insets
+import javafx.stage.DirectoryChooser
+import sliv.tool.importer.controller.ImporterController
+import tornadofx.*
+
+
+class ChoosingDirectoryView : Fragment() {
+    private val controller: ImporterController by inject()
+
+    private val directoryChooser = DirectoryChooser()
+    override val root = hbox(20) {
+        padding = Insets(5.0, 0.0, 0.0, 200.0)
+        label {
+            bind(controller.directoryPath)
+        }
+        button("Change"){
+            action{
+                directoryChooser.title = ("Choose working directory")
+                val dir = directoryChooser.showDialog(currentStage)
+                controller.directoryPath.value = dir.absolutePath
+            }
+        }
+    }
+
+
+
+
+
+    }
+
