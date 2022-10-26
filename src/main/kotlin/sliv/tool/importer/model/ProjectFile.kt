26500cf7ca20fa587dc2283187ac809e4196eafa
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-10-26 22:53:48 +0300

    changed class

--- src/main/kotlin/sliv/tool/importer/model/ProjectFile.kt
+++ src/main/kotlin/sliv/tool/importer/model/ProjectFile.kt
@@ -2,6 +2,6 @@ package sliv.tool.importer.model
 
 import java.nio.file.Path
 
-open class ProjectFile(val name: String, val directoryPath: Path) {
-    
+sealed class ProjectFile(val name: String, val directoryPath: Path) {
+
 }
