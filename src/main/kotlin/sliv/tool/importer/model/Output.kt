3660b1e02db109d750f2cf6b52ff3baaa1f5b6fe
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-10-26 23:08:56 +0300

    rebase and replacing a class

--- src/main/kotlin/sliv/tool/importer/model/Output.kt
+++ src/main/kotlin/sliv/tool/importer/model/Output.kt
@@ -1,6 +1,7 @@
 package sliv.tool.importer.model
 
+import sliv.tool.project.model.LayerKind
 import java.nio.file.Path
 
-class Output(name: String, directoryPath: Path, type: LandmarkType) : ProjectFile(name, directoryPath) {
+class Output(name: String, directoryPath: Path, val type: LayerKind) : ProjectFile(name, directoryPath) {
 }
