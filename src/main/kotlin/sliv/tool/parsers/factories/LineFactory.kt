cbd252ab50e392e5abd6a2e53d61854edfacc7ff
Alexander-Ploskin <ploskin0107@gmail.com>
2022-11-03 12:50:03 +0300

    Layer type is generic too

--- src/main/kotlin/sliv/tool/parsers/factories/LineFactory.kt
+++ src/main/kotlin/sliv/tool/parsers/factories/LineFactory.kt
@@ -5,8 +5,8 @@ import sliv.tool.scene.model.Layer
 import sliv.tool.parsers.structures.Line as StorageLine
 import sliv.tool.scene.model.Landmark.Line as SceneLine
 
-object LineFactory : LandmarkFactory<StorageLine, SceneLine> {
-    override fun buildLandmark(storageFormatLandmark: StorageLine, containingLayer: Layer): SceneLine {
+object LineFactory : LandmarkFactory<StorageLine, SceneLine, Layer.LineLayer> {
+    override fun buildLandmark(storageFormatLandmark: StorageLine, containingLayer: Layer.LineLayer): SceneLine {
         val startPoint = ParserUtils.doubleCoordinatesToScenePoint(storageFormatLandmark.x0, storageFormatLandmark.y0)
         val endPoint = ParserUtils.doubleCoordinatesToScenePoint(storageFormatLandmark.x1, storageFormatLandmark.y1)
 
--- src/main/kotlin/sliv/tool/parsers/factories/PointFactory.kt
+++ src/main/kotlin/sliv/tool/parsers/factories/PointFactory.kt
@@ -5,8 +5,8 @@ import sliv.tool.scene.model.Layer
 import sliv.tool.parsers.structures.Point as StoragePoint
 import sliv.tool.scene.model.Landmark.Keypoint as ScenePoint
 
-object PointFactory : LandmarkFactory<StoragePoint, ScenePoint> {
-    override fun buildLandmark(storageFormatLandmark: StoragePoint, containingLayer: Layer): ScenePoint {
+object PointFactory : LandmarkFactory<StoragePoint, ScenePoint, Layer.PointLayer> {
+    override fun buildLandmark(storageFormatLandmark: StoragePoint, containingLayer: Layer.PointLayer): ScenePoint {
         val point = ParserUtils.doubleCoordinatesToScenePoint(storageFormatLandmark.x, storageFormatLandmark.y)
 
         return ScenePoint(storageFormatLandmark.uid, containingLayer, point)
