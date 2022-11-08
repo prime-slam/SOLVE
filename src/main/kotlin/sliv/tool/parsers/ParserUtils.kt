24f0a57c2ea304e8d90eec335df2c53ddaae5c09
Stanislav Mishchenko <mi.sts@yandex.ru>
2022-11-08 13:15:26 +0300

    Rewrite solution for several colors

--- src/main/kotlin/sliv/tool/parsers/ParserUtils.kt
+++ src/main/kotlin/sliv/tool/parsers/ParserUtils.kt
@@ -1,20 +1,48 @@
 package sliv.tool.parsers
 
+import sliv.tool.parsers.planes.ImagePlanesParser
 import sliv.tool.scene.model.Point
+import java.awt.image.BufferedImage
 import java.io.File
-import java.io.FileNotFoundException
+import java.io.IOException
+import javax.imageio.ImageIO
 
 // Utility functions used in the parser module.
 object ParserUtils {
     fun readFileText(filePath: String): String? {
+        val file = File(filePath)
+
+        if (!file.canRead()) {
+            println("The text file cannot be read!")
+            return null
+        }
+
         var text: String? = null
         try {
-            text = File(filePath).readText()
-        } catch (exception: FileNotFoundException) {
-            println("File not found while reading the text!\n${exception.message}")
+            text = file.readText()
+        } catch (exception: IOException) {
+            println("Input error while reading the text!\n${exception.message}")
         }
 
         return text
+    }
+
+    fun loadImage(filePath: String): BufferedImage? {
+        val file = File(filePath)
+
+        if (!file.canRead()) {
+            println("The image file cannot be read!")
+            return null
+        }
+
+        var image: BufferedImage? = null
+        try {
+            image = ImageIO.read(file)
+        } catch (exception: IOException) {
+            println("Input error while loading the image!\n${exception.message}")
+        }
+
+        return image
     }
 
     fun doubleCoordinatesToScenePoint(x: Double, y: Double) = Point(x.toInt().toShort(), y.toInt().toShort())
