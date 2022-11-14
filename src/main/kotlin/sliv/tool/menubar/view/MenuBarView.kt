8d227f7974ede89bfc3970e9b38090f477f8802c
Alexander-Ploskin <ploskin0107@gmail.com>
2022-11-14 22:18:48 +0300

    Test data importing

--- src/main/kotlin/sliv/tool/menubar/view/MenuBarView.kt
+++ src/main/kotlin/sliv/tool/menubar/view/MenuBarView.kt
@@ -1,14 +1,17 @@
 package sliv.tool.menubar.view
 
 import sliv.tool.importer.view.ImporterView
+import sliv.tool.main.MainController
 import tornadofx.*
 
 class MenuBarView : View() {
+    private val mainController: MainController by inject()
 
     override val root = hbox {
         button("Import project") {
             action {
                 ImporterView().openModal()
+                mainController.importTestData()
             }
         }
         button("Manage plugins") {
