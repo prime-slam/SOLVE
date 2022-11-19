7ccd48d4a46055e741a2eff77acf25fd39a3bbc7
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-11-19 15:51:39 +0300

    added part of parser and other mini fixes

--- src/main/kotlin/sliv/tool/importer/ProjectParser.kt
+++ src/main/kotlin/sliv/tool/importer/ProjectParser.kt
@@ -2,37 +2,63 @@ package sliv.tool.importer
 
 import javafx.collections.FXCollections
 import javafx.scene.control.TreeItem
+import sliv.tool.parsers.planes.ImagePlanesParser.extractUIDs
+import sliv.tool.project.model.LandmarkFile
+import sliv.tool.project.model.LayerKind
+import sliv.tool.project.model.ProjectLayer
 import java.io.File
+import kotlin.io.path.Path
 
 object ProjectParser {
+    const val img = "images"
 
-    fun createTree (path: String?, tree: TreeItem<String>): TreeItem<String> {
-        val files = FXCollections.observableArrayList<Pair<String, String>>()
+    fun createTreeWithFiles(path: String?, tree: TreeItem<String>): TreeItem<String> {
+        val files = FXCollections.observableArrayList<Triple<String, String, String>>()
         val images = FXCollections.observableArrayList<Pair<String, String>>()
 
+        if (path == null) {
+            throw Exception("Directory not selected")
+        }
+
         for (folder in File(path).listFiles()) {
-            if (folder.name == "images") {
+            if (folder.name == img) {
                 for (file in folder.listFiles()){
-                    images.add(Pair(file.nameWithoutExtension, file.absolutePath))
+                    images.add(file.nameWithoutExtension to file.absolutePath)
                 }}
             else{
                 for (file in folder.listFiles()){
-                    files.add(Pair(folder.nameWithoutExtension, file.absolutePath))
+                    files.add(Triple(folder.nameWithoutExtension, file.nameWithoutExtension, file.absolutePath))
                 }
             }
         }
-        if (path != null) {
-            tree.value = path.split("\\").last()
-        }
-        images.forEach { thisImg ->
+
+        tree.value = path.split("/").last()
+        images.map{ thisImg ->
             var img = TreeItem(thisImg.first)
-            files.forEach{ thisFile ->
-                if (thisImg.first == File(thisFile.second).nameWithoutExtension) {
+            files.map{ thisFile ->
+                if (thisImg.first == thisFile.second) {
                     img.children.add(TreeItem(thisFile.first))
                 }
             }
             tree.children.add(img)
+
         }
         return tree
+    }
+
+    fun parse() {
+        val layers = FXCollections.observableArrayList<ProjectLayer>()
+        val landmarks = FXCollections.observableArrayList<LandmarkFile>()
+        val files = FXCollections.observableArrayList<Triple<String, String, String>>()
+        files.forEach{
+             var name = it.first
+             var kind = it.second.split("_")[1]
+             when(kind){
+                 "keypoint" -> layers.add(ProjectLayer(LayerKind.KEYPOINT, name))
+                 "plane" -> layers.add(ProjectLayer(LayerKind.PLANE, name))
+                 "line" -> layers.add(ProjectLayer(LayerKind.LINE, name))
+             }
+             landmarks.add(LandmarkFile(layers.last(), Path(it.third), extractUIDs(it.third) ))
+         }
     }
 }
