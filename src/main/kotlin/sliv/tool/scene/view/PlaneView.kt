0a561a4711a037222ffce14448c80b994d2b0cbf
Alexander-Ploskin <ploskin0107@gmail.com>
2022-11-21 22:09:00 +0300

    Remove commented code

--- src/main/kotlin/sliv/tool/scene/view/PlaneView.kt
+++ src/main/kotlin/sliv/tool/scene/view/PlaneView.kt
@@ -10,21 +10,4 @@ class PlaneView(private val plane: Landmark.Plane, scale: Double) : LandmarkView
     override fun scaleChanged() {
         TODO("Not yet implemented")
     }
-//    override fun draw(gc: GraphicsContext, scale: Double) {
-//        gc.fill = Color.RED //TODO: use some colorizing delegate here
-//        gc.globalAlpha = plane.layer.opacity
-//        when (state) {
-//            LandmarkState.Ordinary -> {
-//                plane.points.forEach {
-//                    gc.fillRect(it.x.toDouble() * scale, it.y.toDouble() * scale, scale, scale)
-//                }
-//            }
-//
-//            LandmarkState.Hovered -> TODO("Draw hovered plane")
-//        }
-//    }
-//
-//    override fun isHovered(x: Double, y: Double, scale: Double): Boolean {
-//        TODO("Not yet implemented")
-//    }
 }
