12e2c7fb5f65bfb3a06c8d8d9d74e6a93d2637b1
Alexander-Ploskin <ploskin0107@gmail.com>
2022-11-21 22:10:41 +0300

    Actualize comment

--- src/main/kotlin/sliv/tool/scene/view/LandmarkView.kt
+++ src/main/kotlin/sliv/tool/scene/view/LandmarkView.kt
@@ -3,9 +3,8 @@ package sliv.tool.scene.view
 import javafx.scene.shape.Shape
 import sliv.tool.scene.model.*
 
-// Landmarks are not presented as visual node, this class is responsive for drawing and styling for landmarks
+// Responsive for creating and setting visual effects for landmarks presenting controls
 // This has access to landmark data class and its layer
-// Due to possible performance issues it is not allowed to handle any events or receive any notifications
 sealed class LandmarkView(scale: Double) {
     companion object {
         fun create(landmark: Landmark, scale: Double): LandmarkView {
