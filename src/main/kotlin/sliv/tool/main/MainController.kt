b2cfe8331a5be83d16163349019aeb05c42f3792
Stanislav Mishchenko <mi.sts@yandex.ru>
2022-11-22 00:38:35 +0300

    Add a test for a selection visualization

--- src/main/kotlin/sliv/tool/main/MainController.kt
+++ src/main/kotlin/sliv/tool/main/MainController.kt
@@ -1,5 +1,10 @@
 package sliv.tool.main
 
+import java.nio.file.Paths
+import java.time.LocalDateTime
+import java.time.ZoneId
+import sliv.tool.catalogue.controller.CatalogueController
+import sliv.tool.project.model.*
 import sliv.tool.scene.SceneFacade
 import sliv.tool.scene.controller.SceneController
 import tornadofx.*
@@ -7,4 +12,17 @@ import tornadofx.*
 class MainController : Controller() {
     private val sceneController: SceneController by inject()
     val sceneFacade = SceneFacade(sceneController)
+    private val catalogueController: CatalogueController by inject()
+
+    //TODO: temporary solution to fill scene with data
+    fun importTestData() {
+        val layer = ProjectLayer(LayerKind.KEYPOINT, "ORB")
+        val path = Paths.get("test/data/room.jpg")
+        val currentTimestamp = {
+            LocalDateTime.now().atZone(ZoneId.of("UTC+03:00")).toInstant().toEpochMilli()
+        }
+        val landmarks = listOf(LandmarkFile(layer, Paths.get("test/data/points.csv"), emptyList()))
+        val frames = (1..30000).map { ProjectFrame(currentTimestamp(), path, landmarks) }
+        catalogueController.displayFrames(frames)
+    }
 }
