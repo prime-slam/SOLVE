1a720b955195e8e6a0c9630e06206efd92e37fc4
Alexander-Ploskin <ploskin0107@gmail.com>
2022-11-21 22:06:02 +0300

    Draw lines

--- src/main/kotlin/sliv/tool/scene/view/LineView.kt
+++ src/main/kotlin/sliv/tool/scene/view/LineView.kt
@@ -1,6 +1,6 @@
 package sliv.tool.scene.view
 
-import javafx.scene.shape.Shape
+import javafx.scene.shape.Line
 import sliv.tool.scene.model.Landmark
 
 class LineView(private val line: Landmark.Line, scale: Double) : LandmarkView(scale) {
@@ -8,30 +8,28 @@ class LineView(private val line: Landmark.Line, scale: Double) : LandmarkView(sc
         private const val OrdinaryWidth: Double = 5.0
     }
 
-    override val shape: Shape
-        get() = TODO("Not yet implemented")
+    override val shape: Line = createShape()
+
+    private val startCoordinates
+        get() = Pair(line.startCoordinate.x.toDouble() * scale, line.finishCoordinate.y.toDouble() * scale)
+
+    private val finishCoordinates
+        get() = Pair(line.finishCoordinate.x.toDouble() * scale, line.finishCoordinate.y.toDouble() * scale)
 
     override fun scaleChanged() {
-        TODO("Not yet implemented")
+        shape.startX = startCoordinates.first
+        shape.startY = startCoordinates.second
+        shape.endX = finishCoordinates.first
+        shape.endY = finishCoordinates.second
     }
 
     private var width = OrdinaryWidth
 
-//    override fun draw(gc: GraphicsContext, scale: Double) {
-//        gc.fill = line.layer.getColor(line)
-//        gc.globalAlpha = line.layer.opacity
-//        val x1 = line.startCoordinate.x.toDouble() * scale
-//        val y1 = line.startCoordinate.y.toDouble() * scale
-//        val x2 = line.finishCoordinate.y.toDouble() * scale
-//        val y2 = line.finishCoordinate.y.toDouble() * scale
-//        gc.lineWidth = width
-//        when (state) {
-//            LandmarkState.Ordinary -> gc.strokeLine(x1, y1, x2, y2)
-//            LandmarkState.Hovered -> TODO("Draw hovered line")
-//        }
-//    }
-//
-//    override fun isHovered(x: Double, y: Double, scale: Double): Boolean {
-//        TODO("Check if the point within the line")
-//    }
+    private fun createShape(): Line {
+        val shape = Line(startCoordinates.first, startCoordinates.second, finishCoordinates.first, finishCoordinates.second)
+        shape.fill = line.layer.getColor(line)
+        shape.opacity = line.layer.opacity
+        shape.strokeWidth = width
+        return shape
+    }
 }
