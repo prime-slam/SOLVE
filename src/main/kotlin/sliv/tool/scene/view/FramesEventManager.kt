57de999be05f48ad71716f74008e4d770658cede
Alexander-Ploskin <ploskin0107@gmail.com>
2022-11-24 17:46:11 +0300

    Add C#-like event class

--- src/main/kotlin/sliv/tool/scene/view/FramesEventManager.kt
+++ src/main/kotlin/sliv/tool/scene/view/FramesEventManager.kt
@@ -1,25 +1,15 @@
 package sliv.tool.scene.view
 
-class Event<T> {
-    private val observers = mutableSetOf<(T) -> Unit>()
-
-    operator fun plusAssign(observer: (T) -> Unit) {
-        observers.add(observer)
-    }
-
-    operator fun minusAssign(observer: (T) -> Unit) {
-        observers.remove(observer)
-    }
-
-    operator fun invoke(eventArgs: T) {
-        for (observer in observers)
-            observer(eventArgs)
-    }
-}
+import sliv.tool.common.*
 
 data class LandmarkEventArgs(val uid: Long, val frameTimestamp: Long)
 
 object FramesEventManager {
     val LandmarkSelected = Event<LandmarkEventArgs>()
     val LandmarkUnselected = Event<LandmarkEventArgs>()
+
+    fun resetListeners() {
+        LandmarkSelected.clear()
+        LandmarkUnselected.clear()
+    }
 }
