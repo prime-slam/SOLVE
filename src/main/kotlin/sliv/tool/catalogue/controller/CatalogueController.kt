29356dae6d1c0d88a0fdc61cc68e90d57e21cb14
Stanislav Mishchenko <mi.sts@yandex.ru>
2022-11-30 14:20:35 +0300

    Add a service locator

--- src/main/kotlin/sliv/tool/catalogue/controller/CatalogueController.kt
+++ src/main/kotlin/sliv/tool/catalogue/controller/CatalogueController.kt
@@ -4,20 +4,19 @@ import sliv.tool.catalogue.layers
 import sliv.tool.catalogue.model.CatalogueModel
 import sliv.tool.project.model.ProjectFrame
 import sliv.tool.scene.SceneFacade
-import sliv.tool.scene.controller.SceneController
+import sliv.tool.utils.ServiceLocator
 import tornadofx.Controller
 
 class CatalogueController: Controller() {
     val model = CatalogueModel()
-
-    private val sceneController: SceneController by inject()
-    private val sceneFacade = SceneFacade(sceneController)
 
     fun displayFrames(frames: List<ProjectFrame>) {
         model.reinitializeFrames(frames)
     }
 
     fun visualizeFramesSelection(frames: List<ProjectFrame>) {
+        val sceneFacade = ServiceLocator.getService<SceneFacade>() ?: return
+
         val layers = frames.flatMap { it.layers }.distinct()
         sceneFacade.visualize(layers, frames)
     }
