2f4d49323ca027a8475e8cb21fed8bdde513d876
Stanislav Mishchenko <mi.sts@yandex.ru>
2022-12-01 01:17:52 +0300

    Add drag and drop

--- src/main/kotlin/sliv/tool/catalogue/CatalogueUtils.kt
+++ src/main/kotlin/sliv/tool/catalogue/CatalogueUtils.kt
@@ -1,11 +1,16 @@
 package sliv.tool.catalogue
 
+import io.github.palexdev.mfxcore.utils.fx.SwingFXUtils.fromFXImage
+import io.github.palexdev.mfxcore.utils.fx.SwingFXUtils.toFXImage
+import javafx.geometry.Point2D
 import javafx.scene.control.ListView
-import javafx.scene.control.Toggle
-import javafx.scene.control.ToggleGroup
+import javafx.scene.image.Image
 import sliv.tool.project.model.ProjectFrame
 import sliv.tool.project.model.ProjectLayer
+import tornadofx.View
 import tornadofx.onChange
+import java.awt.image.BufferedImage
+import kotlin.math.ceil
 
 fun <T> ListView<T>.selectAllItems() = this.selectionModel.selectAll()
 
@@ -17,6 +22,9 @@ val <T> ListView<T>.selectedItems: List<T>
 val <T> ListView<T>.selectedItemsCount: Int
     get() = this.selectedItems.count()
 
+val <T> ListView<T>.selectedIndices: List<Int>
+    get() = this.selectionModel.selectedIndices
+
 fun <T> ListView<T>.selectItem(item: T) = this.selectionModel.select(item)
 
 val ProjectFrame.layers: List<ProjectLayer>
@@ -26,4 +34,25 @@ inline fun <T> ListView<T>.onSelectionChanged(crossinline action: () -> Unit) {
     this.selectionModel.selectedItemProperty().onChange {
         action()
     }
+}
+
+fun View.containsPoint(x: Double, y: Double) = this.root.contains(Point2D(x, y))
+
+fun Double.ceil(): Int = ceil(this).toInt()
+
+fun combineImagesVertically(images: List<Image>): Image {
+    val combinedImageWidth = images.maxOf { it.width }.ceil()
+    val combinedImageHeight = images.sumOf { it.height }.ceil()
+    val combinedImage = BufferedImage(combinedImageWidth, combinedImageHeight, BufferedImage.TYPE_INT_ARGB)
+
+    val graphics = combinedImage.createGraphics()
+    var drawYOffset = 0
+    images.forEach {
+        val bufferedImage = fromFXImage(it, null)
+        graphics.drawImage(bufferedImage, 0, drawYOffset, null)
+        drawYOffset += it.height.ceil()
+    }
+    graphics.dispose()
+
+    return toFXImage(combinedImage, null)
 }
