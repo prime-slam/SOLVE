c4d3851eded42986bd3952e8421035177d952bf7
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-12-04 12:32:33 +0300

    fixed bugs

--- src/main/kotlin/sliv/tool/importer/view/ChoosingDirectoryView.kt
+++ src/main/kotlin/sliv/tool/importer/view/ChoosingDirectoryView.kt
@@ -8,9 +8,10 @@ import sliv.tool.importer.ProjectParser
 import sliv.tool.importer.ProjectParser.parseDirectory
 import sliv.tool.importer.controller.ImporterController
 import sliv.tool.main.MainController
+import sliv.tool.menubar.view.MenuBarView
 import sliv.tool.project.model.Project
 import tornadofx.*
-import sliv.tool.menubar.view.MenuBarView
+import java.io.File
 
 class ChoosingDirectoryView : Fragment() {
     private val controller: ImporterController by inject()
@@ -19,45 +20,74 @@ class ChoosingDirectoryView : Fragment() {
     private val directoryChooser = DirectoryChooser().apply { title = "Choose working directory" }
     private val project = objectProperty<Project>()
 
-    override val root = vbox(20) {
-        var rootTree = TreeItem("Project not selected").apply { isExpanded = true }
-        padding = Insets(5.0, 10.0, 0.0, 10.0)
+    override val root = borderpane {
+        var rootTree = TreeItem("").apply { isExpanded = true }
+        padding = Insets(5.0, 15.0, 0.0, 0.0)
         path.bindBidirectional(controller.directoryPath)
         project.bindBidirectional(controller.project)
-        hbox(20) {
-            label {
-                bind(controller.directoryPath)
-            }
-            button("Change") {
-                action {
-                    val dir = directoryChooser.showDialog(currentStage)
-                    controller.directoryPath.set(dir.absolutePath)
-                }
-            }
-        }
-
-        path.onChange {
-            project.set(parseDirectory(it.toString()))
-            rootTree = ProjectParser.createTreeWithFiles(project.value, rootTree)
-        }
-
-        treeview(rootTree) {
-            visibleWhen { path.isNotEmpty }
-        }
-
-        vbox {
-            padding = Insets(0.0, 0.0, 0.0, 10.0)
-            button("Import") {
-                isDisable = true
-                project.onChange {
-                    if (project.value.frames.isNotEmpty()) {
-                        isDisable = false
+        top {
+            borderpane {
+                left {
+                    vbox {
+                        label("Project directory")
+                        label {
+                            bind(controller.directoryPath)
+                        }
                     }
                 }
-                action {
-                    val projectVal = project.value
-                    mainController.sceneFacade.visualize(projectVal.layers, projectVal.frames)
-                    MenuBarView().importer.close()
+                right {
+                    button("Change") {
+                        action {
+                            val dir = directoryChooser.showDialog(currentStage)
+                            controller.directoryPath.set(dir.absolutePath)
+                        }
+                    }
+                }
+
+            }
+        }
+        path.onChange {
+            if (!it.isNullOrEmpty()) {
+                project.set(parseDirectory(it.toString()))
+                directoryChooser.initialDirectory = File(path.value)
+                rootTree.children.remove(0, rootTree.children.size)
+                rootTree = ProjectParser.createTreeWithFiles(project.value, rootTree)
+            }
+        }
+        center {
+            treeview(rootTree) {
+                visibleWhen { path.isNotEmpty }
+            }
+        }
+        bottom {
+            borderpane {
+
+                padding = Insets(10.0, 10.0, 0.0, 10.0)
+                left {
+                    button("Cancel") {
+                        action {
+                            controller.directoryPath.set(null)
+                            MenuBarView().importer.close()
+
+                        }
+                        prefWidth = 180.0
+                    }
+                }
+                right {
+                    button("Import") {
+                        prefWidth = 180.0
+                        isDisable = true
+                        project.onChange {
+                            if (project.value.frames.isNotEmpty()) {
+                                isDisable = false
+                            }
+                        }
+                        action {
+                            val projectVal = project.value
+                            mainController.sceneFacade.visualize(projectVal.layers, projectVal.frames)
+                            MenuBarView().importer.close()
+                        }
+                    }
                 }
             }
         }
