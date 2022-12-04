171be4cd7c1a5253f8d0ca6e4f3694c5b56bcd39
Stanislav Mishchenko <mi.sts@yandex.ru>
2022-12-04 19:13:47 +0300

    Extra imports

--- src/main/kotlin/sliv/tool/main/MainController.kt
+++ src/main/kotlin/sliv/tool/main/MainController.kt
@@ -1,14 +1,9 @@
 package sliv.tool.main
 
-import sliv.tool.catalogue.controller.CatalogueController
-import sliv.tool.project.model.*
 import sliv.tool.scene.SceneFacade
 import sliv.tool.scene.controller.SceneController
 import sliv.tool.utils.ServiceLocator
 import tornadofx.*
-import java.nio.file.Paths
-import java.time.LocalDateTime
-import java.time.ZoneId
 
 class MainController : Controller() {
     private val sceneController: SceneController by inject()
