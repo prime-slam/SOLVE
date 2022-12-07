ef723aca93b2dbf1ca80cdebf7047e34d55a1885
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2022-12-07 10:01:54 +0300

    fixed some bugs and added displaying errors from above

--- src/main/kotlin/solve/menubar/view/MenuBarView.kt
+++ src/main/kotlin/solve/menubar/view/MenuBarView.kt
@@ -1,16 +1,19 @@
 package solve.menubar.view
 
+import solve.importer.controller.ImporterController
 import solve.importer.view.ImporterView
-import solve.main.MainController
 import tornadofx.*
 
 class MenuBarView : View() {
     val importer = find<ImporterView>()
+    val controller: ImporterController by inject()
 
     override val root = hbox {
         button("Import project") {
             action {
+                controller.directoryPath.set(null)
                 importer.openModal()
+                importer.onDockListeners.apply { minWidth = 50.0 }
             }
         }
         button("Manage plugins") {
