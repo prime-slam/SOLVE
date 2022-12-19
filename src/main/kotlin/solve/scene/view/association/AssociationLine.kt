b9247f18e3d60dc3476284d96217d86889d86838
Alexander-Ploskin <ploskin0107@gmail.com>
2022-12-19 14:24:56 +0300

    implement association line appearance in a separate class

---
+++ src/main/kotlin/solve/scene/view/association/AssociationLine.kt
@@ -0,0 +1,39 @@
+package solve.scene.view.association
+
+import javafx.beans.property.DoubleProperty
+import javafx.scene.Node
+import javafx.scene.paint.Color
+import javafx.scene.shape.Line
+import solve.scene.model.Landmark
+import tornadofx.*
+
+class AssociationLine(
+    private val firstFramePosition: Pair<Double, Double>,
+    private val secondFramePosition: Pair<Double, Double>,
+    private val firstKeypoint: Landmark.Keypoint,
+    private val secondKeypoint: Landmark.Keypoint,
+    private val scale: DoubleProperty
+) {
+    private val line = Line()
+    val node: Node = line
+
+    init {
+        updateLinePosition()
+        line.stroke = AssociationLineColor
+
+        scale.onChange {
+            updateLinePosition()
+        }
+    }
+
+    private fun updateLinePosition() {
+        line.startX = (firstFramePosition.first + firstKeypoint.coordinate.x) * scale.value
+        line.startY = (firstFramePosition.second + firstKeypoint.coordinate.y) * scale.value
+        line.endX = (secondFramePosition.first + secondKeypoint.coordinate.x) * scale.value
+        line.endY = (secondFramePosition.second + secondKeypoint.coordinate.y) * scale.value
+    }
+
+    companion object {
+        private val AssociationLineColor = Color.RED
+    }
+}
