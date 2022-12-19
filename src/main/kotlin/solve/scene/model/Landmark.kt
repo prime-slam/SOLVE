f81407f8c5163c40e5262705135f5ad71d5c37ed
Alexander-Ploskin <ploskin0107@gmail.com>
2022-12-19 19:35:48 +0300

    refactor lines

--- src/main/kotlin/solve/scene/model/Landmark.kt
+++ src/main/kotlin/solve/scene/model/Landmark.kt
@@ -1,9 +1,16 @@
 package solve.scene.model
 
 sealed class Landmark(val uid: Long, open val layerSettings: LayerSettings) {
-    class Keypoint(uid: Long, override val layerSettings: LayerSettings.PointLayerSettings, val coordinate: Point) : Landmark(uid, layerSettings)
-    class Line(uid: Long, override val layerSettings: LayerSettings.LineLayerSettings, val startCoordinate: Point, val finishCoordinate: Point) :
+    class Keypoint(uid: Long, override val layerSettings: LayerSettings.PointLayerSettings, val coordinate: Point) :
         Landmark(uid, layerSettings)
 
-    class Plane(uid: Long, override val layerSettings: LayerSettings.PlaneLayerSettings, val points: List<Point>) : Landmark(uid, layerSettings)
+    class Line(
+        uid: Long,
+        override val layerSettings: LayerSettings.LineLayerSettings,
+        val startCoordinate: Point,
+        val finishCoordinate: Point
+    ) : Landmark(uid, layerSettings)
+
+    class Plane(uid: Long, override val layerSettings: LayerSettings.PlaneLayerSettings, val points: List<Point>) :
+        Landmark(uid, layerSettings)
 }
