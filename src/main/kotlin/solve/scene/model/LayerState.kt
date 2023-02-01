aeb16f507c59179f01ee12688847791f731d5921
Alexander-Ploskin <ploskin0107@gmail.com>
2023-02-01 18:44:52 +0300

    Comment code

--- src/main/kotlin/solve/scene/model/LayerState.kt
+++ src/main/kotlin/solve/scene/model/LayerState.kt
@@ -2,6 +2,7 @@ package solve.scene.model
 
 import tornadofx.toObservable
 
+// Contains landmarks settings which shouldn't be reused when scene is recreated
 class LayerState(val name: String) {
     val selectedLandmarksUids = mutableSetOf<Long>().toObservable()
     val hoveredLandmarksUids = mutableSetOf<Long>().toObservable()
