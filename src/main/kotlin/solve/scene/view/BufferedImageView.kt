14ae953dde285d66a1714b7bc2b9fc4d98011fd1
Alexander-Ploskin <ploskin0107@gmail.com>
2023-02-19 22:01:28 +0300

    Codestyle

--- src/main/kotlin/solve/scene/view/BufferedImageView.kt
+++ src/main/kotlin/solve/scene/view/BufferedImageView.kt
@@ -7,11 +7,8 @@ import solve.utils.ceilToInt
 import java.nio.IntBuffer
 
 class BufferedImageView(
-    private val initialWidth: Double,
-    private val initialHeight: Double,
-    initialScale: Double
-) :
-    ImageView() {
+    private val initialWidth: Double, private val initialHeight: Double, initialScale: Double
+) : ImageView() {
     private val buffer = IntBuffer.allocate(initialWidth.ceilToInt() * initialHeight.ceilToInt())
     private val pixelFormat = PixelFormat.getIntArgbPreInstance()
     private val pixelBuffer = PixelBuffer(initialWidth.ceilToInt(), initialHeight.ceilToInt(), buffer, pixelFormat)
@@ -64,10 +61,6 @@ class BufferedImageView(
         pixelBuffer.updateBuffer { null }
     }
 
-    private fun Color.toArgb(): Int {
-        return 255 shl 24 or
-                ((red * 255).toInt() shl 16) or
-                ((green * 255).toInt() shl 8) or
-                (blue * 255).toInt()
-    }
+    private fun Color.toArgb() =
+        255 shl 24 or ((red * 255).toInt() shl 16) or ((green * 255).toInt() shl 8) or (blue * 255).toInt()
 }
