598d475ef8966ee2cf0d4f8042cc683628033c30
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-02-22 00:57:40 +0300

    added unit tests

--- src/main/kotlin/solve/importer/model/FileInTree.kt
+++ src/main/kotlin/solve/importer/model/FileInTree.kt
@@ -2,4 +2,15 @@ package solve.importer.model
 
 class FileInTree(
     val file: FileInfo
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as FileInTree
+        return file == other.file
+    }
+
+    override fun hashCode(): Int {
+        return file.hashCode()
+    }
+}
