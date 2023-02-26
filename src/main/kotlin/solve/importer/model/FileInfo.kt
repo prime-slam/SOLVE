4c0758b97a6e224f8670604a0d0190ad3ef5aef8
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-02-26 19:32:25 +0300

    go to data class

--- src/main/kotlin/solve/importer/model/FileInfo.kt
+++ src/main/kotlin/solve/importer/model/FileInfo.kt
@@ -1,21 +1,7 @@
 package solve.importer.model
 
-data class FileInfo(val name: String = "", val isLeaf: Boolean = false, val errors: MutableList<String> = mutableListOf()) {
-    override fun equals(other: Any?): Boolean {
-
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        other as FileInfo
-        if (name != other.name) return false
-        if (isLeaf != other.isLeaf) return false
-        if (errors != other.errors) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + isLeaf.hashCode()
-        result = 31 * result + errors.hashCode()
-        return result
-    }
-}
+data class FileInfo(
+    val name: String = "",
+    val isLeaf: Boolean = false,
+    val errors: MutableList<String> = mutableListOf()
+)
