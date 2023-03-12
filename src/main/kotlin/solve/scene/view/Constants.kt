917429d54324019acf849b4b42d8dabb5f61dda9
Alexander-Ploskin <ploskin0107@gmail.com>
2023-03-12 23:34:00 +0300

    Remove unused

--- src/main/kotlin/solve/scene/view/Constants.kt
+++ src/main/kotlin/solve/scene/view/Constants.kt
@@ -1,10 +1,6 @@
 package solve.scene.view
 
-import javafx.scene.paint.Color
-
 // Node with a lower viewOrder will be in front of a child with a higher viewOrder
 const val IMAGE_VIEW_ORDER = Int.MAX_VALUE.toDouble()
 const val LANDMARKS_VIEW_ORDER = Int.MAX_VALUE.toDouble() - 1
 const val HIGHLIGHTING_VIEW_ORDER_GAP = Int.MAX_VALUE.toDouble() / 2
-
-val NULL_COLOR = Color(0.0, 0.0, 0.0, 0.0)
