7960daedbd8a09c101e038c4dc77662ce17cb70e
Alexander-Ploskin <ploskin0107@gmail.com>
2023-03-23 10:10:25 +0300

    Fix run issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 application {
     mainClass.set("solve.SolveApp")
-   // applicationDefaultJvmArgs = listOf("--add-opens", "javafx.graphics/javafx.scene=ALL-UNNAMED")
+    applicationDefaultJvmArgs = listOf("--add-opens", "javafx.graphics/javafx.scene=ALL-UNNAMED")
 }
 
 javafx {
