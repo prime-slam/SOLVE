f50a1a5d501d0d0002aa443f6bc45acaac9a3954
Alexander-Ploskin <ploskin0107@gmail.com>
2023-03-26 15:16:42 +0300

    Grid lifetime is less than scaleProperty lifetime

--- src/main/kotlin/solve/scene/view/virtualizedfx/VirtualizedFXGrid.kt
+++ src/main/kotlin/solve/scene/view/virtualizedfx/VirtualizedFXGrid.kt
@@ -1,9 +1,12 @@
 package solve.scene.view.virtualizedfx
 
 import io.github.palexdev.mfxcore.base.beans.Position
+import io.github.palexdev.mfxcore.base.beans.Size
 import io.github.palexdev.mfxcore.base.bindings.MFXBindings
 import io.github.palexdev.virtualizedfx.controls.VirtualScrollPane
 import io.github.palexdev.virtualizedfx.grid.VirtualGrid
+import javafx.beans.InvalidationListener
+import javafx.beans.property.DoubleProperty
 import javafx.beans.property.SimpleDoubleProperty
 import javafx.event.EventHandler
 import javafx.geometry.Orientation
@@ -14,7 +17,11 @@ import solve.scene.view.Grid
 import tornadofx.onChange
 
 class VirtualizedFXGrid(
-    private val virtualGrid: VirtualGrid<VisualizationFrame?, FrameViewAdapter>, private val vsp: VirtualScrollPane
+    private val virtualGrid: VirtualGrid<VisualizationFrame?, FrameViewAdapter>,
+    private val vsp: VirtualScrollPane,
+    private val scaleProperty: DoubleProperty,
+    private val cellWidth: Double,
+    private val cellHeight: Double
 ) : Grid {
     private var dragStartMousePosition = Position.of(-1.0, -1.0)
     private var dragStartGridPosition = Position.of(0.0, 0.0)
@@ -24,7 +31,17 @@ class VirtualizedFXGrid(
     override val xProperty = SimpleDoubleProperty(virtualGrid.position.x)
     override val yProperty = SimpleDoubleProperty(virtualGrid.position.y)
 
+    private val scaleChangedListener = InvalidationListener {
+        // Virtual grid breaks if cell size changes when x position differs from 0.0
+        // Virtual grid always drops position to (0, 0) when cell size changes, so this solution is ok
+        scrollX(0.0)
+        scrollY(0.0)
+        val newScale = scaleProperty.value
+        virtualGrid.cellSize = Size(cellWidth * newScale, cellHeight * newScale)
+    }
+
     init {
+        scaleProperty.addListener(scaleChangedListener)
         virtualGrid.positionProperty().onChange { position ->
             xProperty.set(position?.x ?: 0.0)
             yProperty.set(position?.y ?: 0.0)
@@ -63,5 +80,6 @@ class VirtualizedFXGrid(
         val bindings = MFXBindings.instance()
         bindings.unbindBidirectional(vsp.vValProperty())
         bindings.unbindBidirectional(vsp.hValProperty())
+        scaleProperty.removeListener(scaleChangedListener)
     }
 }
--- src/main/kotlin/solve/scene/view/virtualizedfx/VirtualizedFXGridProvider.kt
+++ src/main/kotlin/solve/scene/view/virtualizedfx/VirtualizedFXGridProvider.kt
@@ -1,16 +1,15 @@
 package solve.scene.view.virtualizedfx
 
-import io.github.palexdev.mfxcore.base.beans.*
+import io.github.palexdev.mfxcore.base.beans.Size
 import io.github.palexdev.mfxcore.collections.ObservableGrid
 import io.github.palexdev.virtualizedfx.grid.VirtualGrid
 import io.github.palexdev.virtualizedfx.utils.VSPUtils
 import javafx.beans.property.DoubleProperty
-import javafx.geometry.Orientation
 import solve.scene.model.VisualizationFrame
-import solve.scene.view.*
+import solve.scene.view.FrameView
+import solve.scene.view.Grid
+import solve.scene.view.GridProvider
 import solve.scene.view.association.OutOfFramesLayer
-import tornadofx.*
-
 
 object VirtualizedFXGridProvider : GridProvider {
     override fun createGrid(
@@ -24,7 +23,7 @@ object VirtualizedFXGridProvider : GridProvider {
     ): Grid {
         val gridData = ObservableGrid.fromList(data, columnsNumber)
         val grid = VirtualGrid(gridData) { item -> FrameViewAdapter(cellFactory(item)) }
-        grid.cellSize = Size(cellWidth, cellHeight)
+        grid.cellSize = Size(cellWidth * scale.value, cellHeight * scale.value)
         grid.prefHeight = Int.MAX_VALUE.toDouble()
 
         val vsp = wrapGridWithVsp(grid, outOfFramesLayer)
@@ -33,20 +32,10 @@ object VirtualizedFXGridProvider : GridProvider {
         // Set up scrolling speed to achieve smooth scrolling
         VSPUtils.setVSpeed(vsp, 100.0, 100.0, 100.0)
 
-        scale.onChange { newScale ->
-            // Virtual grid breaks if cell size changes when x position differs from 0.0
-            // Virtual grid always drops position to (0, 0) when cell size changes, so this solution is ok
-            grid.scrollTo(0.0, Orientation.HORIZONTAL)
-            grid.scrollTo(0.0, Orientation.VERTICAL)
-            grid.cellSize = Size(
-                cellWidth * newScale, cellHeight * newScale
-            )
-        }
-
         grid.setOnScroll { event ->
             event.consume() // Avoid vsp scrolling if mouse is on the grid pane
         }
 
-        return VirtualizedFXGrid(grid, vsp)
+        return VirtualizedFXGrid(grid, vsp, scale, cellWidth, cellHeight)
     }
 }
