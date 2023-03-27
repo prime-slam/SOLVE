4bb2fda62c45f5c8bcf902c576bbe6ae8a4a6697
Alexander-Ploskin <ploskin0107@gmail.com>
2023-03-27 19:53:48 +0300

    Default equals is enough

--- src/main/kotlin/solve/scene/model/Size.kt
+++ src/main/kotlin/solve/scene/model/Size.kt
@@ -1,15 +1,3 @@
 package solve.scene.model
 
-data class Size(val width: Int, val height: Int) {
-    override fun hashCode(): Int {
-        return width shl 16 + height
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
-        val otherSize = other as Size
-        return width == otherSize.width && height == otherSize.height
-    }
-}
+data class Size(val width: Int, val height: Int)
