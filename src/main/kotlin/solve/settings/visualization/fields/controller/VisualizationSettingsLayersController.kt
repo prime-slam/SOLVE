c3dbb365200f6efea2a116f5e6d71b391cc616c7
Stanislav Mishchenko <mi.sts@yandex.ru>
2023-03-28 22:50:20 +0300

    Add a group indices convertion logic

--- src/main/kotlin/solve/settings/visualization/fields/controller/VisualizationSettingsLayersController.kt
+++ src/main/kotlin/solve/settings/visualization/fields/controller/VisualizationSettingsLayersController.kt
@@ -1,5 +1,6 @@
 package solve.settings.visualization.fields.controller
 
+import solve.scene.controller.SceneController
 import solve.scene.model.LayerSettings
 import solve.settings.visualization.fields.model.VisualizationSettingsLayersModel
 import tornadofx.Controller
@@ -7,7 +8,25 @@ import tornadofx.Controller
 class VisualizationSettingsLayersController : Controller() {
     val model = VisualizationSettingsLayersModel()
 
+    private val sceneController: SceneController by inject()
+
     fun setLayerFields(layers: List<LayerSettings>) {
         model.reinitializeLayers(layers)
+        initializeSceneLayersIndices(layers)
+    }
+
+    private fun initializeSceneLayersIndices(
+        layers: List<LayerSettings>
+    ) {
+        val firstPlaneLayerSettingsIndex = layers.indexOfFirst { it is LayerSettings.PlaneLayerSettings }
+        val planeLayers = layers.subList(firstPlaneLayerSettingsIndex, layers.count())
+        val notPlaneLayers = layers.subList(0, firstPlaneLayerSettingsIndex)
+
+        notPlaneLayers.forEachIndexed { index, notPlane ->
+            sceneController.scene.changeLayerIndex(notPlane, index)
+        }
+        planeLayers.forEachIndexed { index, plane ->
+            sceneController.scene.changeLayerIndex(plane, index)
+        }
     }
 }
