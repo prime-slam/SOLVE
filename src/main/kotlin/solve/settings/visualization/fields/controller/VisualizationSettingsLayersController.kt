31a1ecfb9ffc67ec662b8495729648b5bfee7fb8
Stanislav Mishchenko <mi.sts@yandex.ru>
2023-03-28 23:47:12 +0300

    Small bug

--- src/main/kotlin/solve/settings/visualization/fields/controller/VisualizationSettingsLayersController.kt
+++ src/main/kotlin/solve/settings/visualization/fields/controller/VisualizationSettingsLayersController.kt
@@ -19,7 +19,10 @@ class VisualizationSettingsLayersController : Controller() {
     private fun initializeSceneLayersIndices(
         layers: List<LayerSettings>
     ) {
-        val firstPlaneLayerSettingsIndex = layers.indexOfFirst { it is LayerSettings.PlaneLayerSettings }
+        var firstPlaneLayerSettingsIndex = layers.indexOfFirst { it is LayerSettings.PlaneLayerSettings }
+        if (firstPlaneLayerSettingsIndex == -1) {
+            firstPlaneLayerSettingsIndex = layers.count()
+        }
         val planeLayers = layers.subList(firstPlaneLayerSettingsIndex, layers.count())
         val notPlaneLayers = layers.subList(0, firstPlaneLayerSettingsIndex)
 
