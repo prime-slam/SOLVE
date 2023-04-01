6d50d233c5a85fc6350f73cc5e2a6ae399aee678
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-04-02 02:41:47 +0300

    added some design for importer

--- src/main/kotlin/solve/importer/view/ImporterView.kt
+++ src/main/kotlin/solve/importer/view/ImporterView.kt
@@ -1,19 +1,31 @@
 package solve.importer.view
 
-import javafx.geometry.Insets
+import javafx.scene.layout.BorderPane
+import javafx.stage.DirectoryChooser
 import javafx.stage.Stage
-import tornadofx.borderpane
+import solve.importer.controller.ImporterController
 import tornadofx.View
 
-class ImporterView : View("Choose working directory") {
+class ImporterView : View() {
+
+    val importer = find<DirectoryPathView>()
+
+    private val directoryChooser = DirectoryChooser().apply { title = "Choose working directory" }
+
+    private val controller: ImporterController by inject()
+
     override fun onDock() {
         (root.scene.window as Stage).minWidth = 390.0
     }
 
-    override val root = borderpane {
-        padding = Insets(8.0, 10.0, 8.0, 10.0)
-        top<DirectoryPathView>()
-        center<ProjectTreeView>()
-        bottom<ControlPanel>()
+    override val root: BorderPane by fxml("/fxml/app.fxml", hasControllerAttribute = true)
+
+    fun changeAction() {
+        val dir = directoryChooser.showDialog(currentStage)
+        controller.directoryPath.set(dir?.absolutePath)
+    }
+
+    fun cancelAction() {
+        this.close()
     }
 }
