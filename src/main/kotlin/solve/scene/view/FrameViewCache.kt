5db7d49efc952cec340dbf25dd8f76ad0c4e7712
Alexander-Ploskin <ploskin0107@gmail.com>
2023-04-04 13:56:47 +0300

    Update order manager when FrameView is reused

--- src/main/kotlin/solve/scene/view/FrameViewCache.kt
+++ src/main/kotlin/solve/scene/view/FrameViewCache.kt
@@ -7,13 +7,13 @@ interface FrameViewStorage {
     fun store(frameView: FrameView)
 }
 
-class FrameViewCache(val size: DoubleSize, val canvasBufferDepth: Int, private val factory: (VisualizationFrame?) -> FrameView): FrameViewStorage {
+class FrameViewCache(val size: DoubleSize, val canvasBufferDepth: Int, private val factory: (VisualizationFrame?, FrameViewParameters) -> FrameView): FrameViewStorage {
     private val storage = mutableSetOf<FrameView>()
 
-    fun get(frame: VisualizationFrame?) = storage.firstOrNull()?.also { frameView ->
-        frameView.init(frame)
+    fun get(frame: VisualizationFrame?, frameViewParameters: FrameViewParameters) = storage.firstOrNull()?.also { frameView ->
+        frameView.init(frame, frameViewParameters)
         storage.remove(frameView)
-    } ?: factory(frame)
+    } ?: factory(frame, frameViewParameters)
 
     override fun store(frameView: FrameView) {
         if (frameView.size != size) {
