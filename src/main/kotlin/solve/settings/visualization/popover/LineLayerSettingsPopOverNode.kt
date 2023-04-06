0d9c005c04907c262926bd5cc5d74eb01ada613c
Stanislav Mishchenko <mi.sts@yandex.ru>
2023-04-06 22:05:06 +0300

    Fix memory-leak listeners in the settings popover

--- src/main/kotlin/solve/settings/visualization/popover/LineLayerSettingsPopOverNode.kt
+++ src/main/kotlin/solve/settings/visualization/popover/LineLayerSettingsPopOverNode.kt
@@ -1,7 +1,7 @@
 package solve.settings.visualization.popover
 
+import javafx.beans.value.WeakChangeListener
 import javafx.scene.Node
-import javafx.scene.control.Slider
 import solve.scene.controller.SceneController
 import solve.scene.model.LayerSettings
 import solve.utils.structures.Alignment
@@ -19,32 +19,23 @@ class LineLayerSettingsPopOverNode(
         const val LineWidthSliderMaxValue = 10.0
     }
 
+    private val widthSliderValueChangedEventHandler = ChangeListener<Number> { _, _, widthValue ->
+        lineLayerSettings.selectedWidth = widthValue as Double
+    }
+    private val weakWidthSliderValueChangedEventHandler = WeakChangeListener(widthSliderValueChangedEventHandler)
+
     override fun getPopOverNode(): Node {
         popOver.setPrefSize(LayerSettingsNodePrefWidth, LayerSettingsNodePrefHeight)
 
         addSettingField("Color", buildLandmarkColorPicker(lineLayerSettings, sceneController))
-        addSettingField("Width", buildLineWidthSlider())
+        addSettingField("Width", buildSizeSlider(
+            lineLayerSettings.selectedWidth,
+            LineWidthSliderMinValue,
+            LineWidthSliderMaxValue,
+            weakWidthSliderValueChangedEventHandler
+        ))
         addSettingField("One color", buildLandmarkUseOneColorCheckBox(lineLayerSettings), Alignment.Left)
 
         return popOver
-    }
-
-    private fun buildLineWidthSlider(): Slider {
-        val slider = Slider()
-
-        val initialSelectedWidth = lineLayerSettings.selectedWidth
-        if (initialSelectedWidth !in LineWidthSliderMinValue..LineWidthSliderMaxValue) {
-            throw IllegalArgumentException("The initial selected line width is out of selection range!")
-        }
-        slider.min = LineWidthSliderMinValue
-        slider.max = LineWidthSliderMaxValue
-        slider.value = lineLayerSettings.selectedWidth
-        slider.isShowTickLabels = true
-
-        slider.valueProperty().onChange { widthValue ->
-            lineLayerSettings.selectedWidth = widthValue
-        }
-
-        return slider
     }
 }
