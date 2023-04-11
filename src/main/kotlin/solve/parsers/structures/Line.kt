78ebf5d9b81b9dabb75374a8ae804ee2e3928243
Stanislav Mishchenko <mi.sts@yandex.ru>
2023-04-11 21:56:48 +0300

    Add additional tests for points

--- src/main/kotlin/solve/parsers/structures/Line.kt
+++ src/main/kotlin/solve/parsers/structures/Line.kt
@@ -1,5 +1,3 @@
 package solve.parsers.structures
 
-data class Line(val uid: Long, val x0: Double, val y0: Double, val x1: Double, val y1: Double) {
-    override fun toString() = "$uid,$x0,$y0,$x1,$y1"
-}
+data class Line(val uid: Long, val x0: Double, val y0: Double, val x1: Double, val y1: Double)
--- src/main/kotlin/solve/parsers/structures/Point.kt
+++ src/main/kotlin/solve/parsers/structures/Point.kt
@@ -1,5 +1,3 @@
 package solve.parsers.structures
 
-data class Point(val uid: Long, val x: Double, val y: Double) {
-    override fun toString() = "$uid,$x,$y"
-}
+data class Point(val uid: Long, val x: Double, val y: Double)
