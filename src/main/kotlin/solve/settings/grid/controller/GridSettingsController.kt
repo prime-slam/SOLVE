aadd12ba4b6d38e50c59772764fafba71a4a9ec1
Stanislav Mishchenko <mi.sts@yandex.ru>
2023-04-12 23:16:09 +0300

    Fix bugs and refacor a code

--- src/main/kotlin/solve/settings/grid/controller/GridSettingsController.kt
+++ src/main/kotlin/solve/settings/grid/controller/GridSettingsController.kt
@@ -1,38 +1,20 @@
 package solve.settings.grid.controller
 
 import solve.scene.controller.SceneController
-import solve.settings.grid.view.GridSettingsView
 import tornadofx.Controller
-import tornadofx.onChange
 
 class GridSettingsController: Controller() {
-    private val view: GridSettingsView by inject()
     private val sceneController: SceneController by inject()
-
-    init {
-        addSceneListeners()
-    }
 
     fun setSceneColumnsNumber(columnsNumber: Int) {
         sceneController.columnsNumber = columnsNumber
     }
 
     fun setSceneMinScale(minScale: Double) {
-        sceneController.scaleLowValue = minScale
+        sceneController.installedMinScale = minScale
     }
 
     fun setSceneMaxScale(maxScale: Double) {
-        sceneController.scaleHighValue = maxScale
-    }
-
-    private fun addSceneListeners() {
-        sceneController.sceneProperty.onChange { newScene ->
-            newScene ?: return@onChange
-
-            val newColumnsNumber = SceneController.calculateColumnsCount(newScene)
-            view.columnsCounterValue = newColumnsNumber
-
-            view.setDefaultScaleRangeSliderValues()
-        }
+        sceneController.installedMaxScale = maxScale
     }
 }
