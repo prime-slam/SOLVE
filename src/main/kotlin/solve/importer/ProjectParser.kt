5bbc64509908998632b5719f6fb6b970826c2e54
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-04-15 14:18:44 +0300

    added materialfx dialogs and fixed some bugs

--- src/main/kotlin/solve/importer/ProjectParser.kt
+++ src/main/kotlin/solve/importer/ProjectParser.kt
@@ -152,6 +152,7 @@ object ProjectParser {
         if (!isImagesExist) {
             createAlertForError("The images folder is missing in the directory")
             return null
+
         }
 
         images.map { img ->
