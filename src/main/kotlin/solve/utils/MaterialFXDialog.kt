fc76dbd5542de8323e546359db25bf65cf916a04
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-04-16 19:14:39 +0300

    moved MaterialFXDialog

---
+++ src/main/kotlin/solve/utils/MaterialFXDialog.kt
@@ -0,0 +1,38 @@
+package solve.utils
+
+import io.github.palexdev.materialfx.dialogs.MFXGenericDialog
+import io.github.palexdev.materialfx.dialogs.MFXGenericDialogBuilder
+import io.github.palexdev.materialfx.dialogs.MFXStageDialog
+import io.github.palexdev.materialfx.enums.ScrimPriority
+import javafx.scene.layout.Pane
+import javafx.stage.Modality
+import javafx.stage.Stage
+
+object MaterialFXDialog {
+    fun createGenericDialog(container: Pane): MFXGenericDialog {
+
+        return MFXGenericDialogBuilder.build()
+            .setContent(container)
+            .setShowMinimize(false)
+            .setShowAlwaysOnTop(false)
+            .setShowClose(false)
+            .get()
+    }
+
+    fun createStageDialog(dialogContent: MFXGenericDialog, owner: Stage?, ownerNode: Pane): MFXStageDialog {
+        return MFXGenericDialogBuilder.build(dialogContent)
+            .toStageDialogBuilder()
+            .initOwner(owner)
+            .initModality(Modality.APPLICATION_MODAL)
+            .setDraggable(true)
+            .setOwnerNode(ownerNode)
+            .setScrimPriority(ScrimPriority.NODE)
+            .setScrimOwner(true)
+            .get()
+    }
+
+    fun changeContent(dialog: MFXGenericDialog, newContent: Pane) {
+        dialog.content = newContent
+    }
+
+}
