bfb65e5c2d56bc86d821de65321e2f6eb5946b11
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-04-16 19:28:01 +0300

    added style instead font

--- src/main/kotlin/solve/importer/view/AlertDialog.kt
+++ src/main/kotlin/solve/importer/view/AlertDialog.kt
@@ -4,16 +4,15 @@ import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.image.ImageView
 import javafx.scene.layout.BorderPane
-import javafx.scene.text.Font
 import kotlinx.coroutines.cancel
 import solve.constants.IconsAlertError
+import solve.styles.Style
 import solve.utils.loadResourcesImage
 import solve.utils.mfxButton
 import tornadofx.*
 
 class AlertDialog(contentText: String) : View() {
-
-    val errorIcon = loadResourcesImage(IconsAlertError)
+    private val errorIcon = loadResourcesImage(IconsAlertError)
 
     override val root =
         borderpane {
@@ -23,13 +22,13 @@ class AlertDialog(contentText: String) : View() {
                     graphic = ImageView(errorIcon)
                     BorderPane.setMargin(this, Insets(0.0,0.0,0.0,14.0))
                     prefHeight=0.0
-                    font = Font.font("Roboto Condensed", 20.0)
+                    style = "-fx-font-style: ${Style.fontCondensed}; -fx-font-size: 20px;"
                 }
             }
             center {
                 label(contentText){
                     BorderPane.setMargin(this, Insets(0.0, 14.0, 0.0, 14.0))
-                    style = "-fx-font-family: Roboto Condensed; -fx-font-size: 14px;  -fx-text-fill: #000000;"
+                    style = "-fx-font-family: ${Style.font}; -fx-font-size: 15px;  -fx-text-fill: #000000;"
                     BorderPane.setAlignment(this, Pos.CENTER_LEFT)
                     isWrapText = true
                     prefWidth = 300.0
@@ -41,7 +40,7 @@ class AlertDialog(contentText: String) : View() {
                     BorderPane.setMargin(this, Insets(0.0, 14.0, 14.0, 0.0))
                     maxWidth = 40.0
                     prefHeight = 23.0
-                    style = "-fx-font-family: Roboto Condensed; -fx-font-size: 14px; -fx-font-weight: Bold; -fx-text-fill: #78909C;"
+                    style = "-fx-font-family: ${Style.fontCondensed}; -fx-font-size: 14px; -fx-font-weight: Bold; -fx-text-fill: #78909C;"
                     BorderPane.setAlignment(this, Pos.TOP_RIGHT)
                     prefWidth = 180.0
                     action {
