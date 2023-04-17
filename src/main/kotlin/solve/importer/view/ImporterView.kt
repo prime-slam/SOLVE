27040cb1018afcb1e9a2f944984d331bb359b9c7
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-04-17 15:32:28 +0300

    added fonts

--- src/main/kotlin/solve/importer/view/ImporterView.kt
+++ src/main/kotlin/solve/importer/view/ImporterView.kt
@@ -8,6 +8,10 @@ class ImporterView : View() {
     val importer = find<DirectoryPathView>()
 
     override val root = borderpane {
+        stylesheets.add("https://fonts.googleapis.com/css2?family=Roboto+Condensed")
+        stylesheets.add("https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700")
+        stylesheets.add("https://fonts.googleapis.com/css2?family=Roboto")
+
         style = "-fx-background-color: #${Style.surfaceColor};"
         prefHeight = 555.0
         prefWidth = 453.0
