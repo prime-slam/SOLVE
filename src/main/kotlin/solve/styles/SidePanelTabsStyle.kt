fdd6408e233ce00e9354eba0b4b0fe4cf22c655d
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-04-23 12:50:41 +0300

    moved style to new file

---
+++ src/main/kotlin/solve/styles/SidePanelTabsStyle.kt
@@ -0,0 +1,54 @@
+package solve.styles
+
+import javafx.scene.paint.Color
+import javafx.scene.paint.Paint
+import solve.constants.IconsProjectFilled
+import solve.constants.IconsLayers
+import solve.constants.IconsLayersFilled
+import solve.constants.IconsProject
+import solve.utils.createPxBoxWithValue
+import tornadofx.Stylesheet
+import tornadofx.cssclass
+import java.net.URI
+
+class SidePanelTabsStyle : Stylesheet() {
+    val project by cssclass()
+    val layers by cssclass()
+
+    init {
+        toggleButton {
+            backgroundColor += Paint.valueOf(Style.surfaceColor)
+            backgroundInsets += createPxBoxWithValue(0.0)
+            backgroundRadius += createPxBoxWithValue(0.0)
+            textFill = Paint.valueOf(Style.primaryColorLight)
+            and(selected) {
+                textFill = Paint.valueOf(Style.primaryColor)
+            }
+        }
+        project {
+            graphic = URI(IconsProject)
+
+            and(hover) {
+                backgroundColor += Paint.valueOf(Style.backgroundColour)
+                focusColor = Color.TRANSPARENT
+            }
+            and(selected) {
+                backgroundColor += Paint.valueOf(Style.surfaceColor)
+                graphic = URI(IconsProjectFilled)
+            }
+        }
+
+        layers {
+            graphic = URI(IconsLayers)
+
+            and(hover) {
+                backgroundColor += Paint.valueOf(Style.backgroundColour)
+                focusColor = Color.TRANSPARENT
+            }
+            and(selected) {
+                backgroundColor += Paint.valueOf(Style.surfaceColor)
+                graphic = URI(IconsLayersFilled)
+            }
+        }
+    }
+}
