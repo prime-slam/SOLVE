19cf20a4eaa35bb42f402f467e7beffe71096945
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-04-28 12:33:53 +0300

    fixed codestyle

--- src/main/kotlin/solve/importer/view/DirectoryPathView.kt
+++ src/main/kotlin/solve/importer/view/DirectoryPathView.kt
@@ -30,25 +30,27 @@ class DirectoryPathView : View() {
         isDisable = true
         textFill = Color.valueOf(Style.onBackgroundColor)
 
-        prefHeight=48.0
-        prefWidth=280.0
+        prefHeight = 48.0
+        prefWidth = 280.0
         BorderPane.setAlignment(this, Pos.CENTER)
-        BorderPane.setMargin(this, Insets(16.0, 0.0,0.0, 0.0))
+        BorderPane.setMargin(this, Insets(16.0, 0.0, 0.0, 0.0))
 
-        style = "-fx-border-color: #${Style.primaryColorLight}; -fx-font-size: ${Style.mainFontSize}; -fx-font-family: ${Style.fontCondensed}"
+        style =
+            "-fx-border-color: #${Style.primaryColorLight}; -fx-font-size: ${Style.mainFontSize}; -fx-font-family: ${Style.fontCondensed}"
 
         floatMode = FloatMode.BORDER
         floatingText = "Project directory"
 
         controller.projectAfterPartialParsing.onChange {
-            if (controller.projectAfterPartialParsing.value != null){
+            if (controller.projectAfterPartialParsing.value != null) {
                 tooltip(controller.directoryPath.value).apply {
-                    style = "-fx-font-family: ${Style.fontCondensed}; -fx-font-size: ${Style.tooltipFontSize}; -fx-background-color: #${Style.surfaceColor}; -fx-text-fill: #707070;"
+                    style =
+                        "-fx-font-family: ${Style.fontCondensed}; -fx-font-size: ${Style.tooltipFontSize}; -fx-background-color: #${Style.surfaceColor}; -fx-text-fill: #707070;"
                 }
             }
-            text = if (it != null){
+            text = if (it != null) {
                 controller.directoryPath.value
-            } else{
+            } else {
                 empty
             }
         }
@@ -56,20 +58,21 @@ class DirectoryPathView : View() {
 
     private val labelName = label("Import a directory") {
         hgrow = Priority.ALWAYS
-        prefHeight=0.0
-        prefWidth=200.0
+        prefHeight = 0.0
+        prefWidth = 200.0
 
         style = "-fx-font-family: ${Style.fontCondensed}; -fx-font-size: ${Style.headerFontSize}; "
 
         VBox.setMargin(this, Insets(0.0, 0.0, 0.0, 0.0))
     }
 
-    private val selectButton = mfxButton ("SELECT"){
-        style="-fx-border-color: #${Style.primaryColor};  -fx-font-size: ${Style.buttonFontSize}; -fx-font-family: ${Style.fontCondensed}; -fx-font-weight:700; -fx-border-radius: 4px; -fx-text-fill: #${Style.primaryColor};"
+    private val selectButton = mfxButton("SELECT") {
+        style =
+            "-fx-border-color: #${Style.primaryColor};  -fx-font-size: ${Style.buttonFontSize}; -fx-font-family: ${Style.fontCondensed}; -fx-font-weight:700; -fx-border-radius: 4px; -fx-text-fill: #${Style.primaryColor};"
         BorderPane.setAlignment(this, Pos.CENTER)
         BorderPane.setMargin(this, Insets(15.0, 0.0, 0.0, 0.0))
         textFill = Color.valueOf(Style.primaryColor)
-        alignment=Pos.CENTER
+        alignment = Pos.CENTER
         depthLevel = DepthLevel.LEVEL1
         prefHeight = 31.0
         prefWidth = 98.0
@@ -98,14 +101,14 @@ class DirectoryPathView : View() {
             BorderPane.setAlignment(this, Pos.CENTER)
             add(labelName)
             borderpane {
-                padding = Insets(0.0, 0.0, 10.0,0.0)
-                prefHeight=0.0
-                prefWidth=453.0
+                padding = Insets(0.0, 0.0, 10.0, 0.0)
+                prefHeight = 0.0
+                prefWidth = 453.0
 
                 right {
                     add(selectButton)
                 }
-                left{
+                left {
                     add(directoryField)
                 }
             }
