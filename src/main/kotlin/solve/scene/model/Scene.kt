d99245c9cf817cec04beeaffb42984b62532d5b2
Alexander-Ploskin <ploskin0107@gmail.com>
2023-05-04 00:21:03 +0300

    Scene tests

--- src/main/kotlin/solve/scene/model/Scene.kt
+++ src/main/kotlin/solve/scene/model/Scene.kt
@@ -2,23 +2,37 @@ package solve.scene.model
 
 import solve.utils.structures.Size as DoubleSize
 
+/**
+ * Aggregates scene data and manages order of landmarks from different layers on frames.
+ *
+ * @param frames images with related landmarks selected in the catalog {@link CatalogueController}.
+ * @param layerSettings information about layers in the project, used to determine current order of a layer.
+ */
 class Scene(
-    val frames: List<VisualizationFrame>,
-    layerSettings: List<LayerSettings>
+    val frames: List<VisualizationFrame>, val layerSettings: List<LayerSettings>
 ) : OrderManager<LayerSettings> {
-    val frameSize = frames.firstOrNull()?.let {
-        val image = it.getImage()
-        DoubleSize(image.width, image.height)
-    } ?: DoubleSize(0.0, 0.0)
+    /**
+     * Size of images in the project. Assumes that all images have the same size, but there is no guarantee.
+     */
+    val frameSize: DoubleSize by lazy {
+        frames.firstOrNull()?.let {
+            val image = it.getImage()
+            DoubleSize(image.width, image.height)
+        } ?: DoubleSize(0.0, 0.0)
+    }
 
-    val canvasLayersCount = layerSettings.count { it.usesCanvas }
+    private val canvasLayersStorage =
+        layerSettings.filter { it.usesCanvas }.toMutableList()
 
-    private val planeLayerSettingsStorage =
-        layerSettings.filterIsInstance<LayerSettings.PlaneLayerSettings>().toMutableList()
-    private val layerSettingsStorage =
-        layerSettings.filterNot { it is LayerSettings.PlaneLayerSettings }.toMutableList()
-
+    /**
+     * Layers which use frame drawer and which don't should be stored separately
+     * to keep canvas layers always above non-canvas.
+     */
+    private val nonCanvasLayersStorage =
+        layerSettings.filterNot { it.usesCanvas }.toMutableList()
     private val changedCallbacks = mutableListOf<() -> Unit>()
+
+    val canvasLayersCount = canvasLayersStorage.size
 
     override fun addOrderChangedListener(action: () -> Unit) {
         changedCallbacks.add(action)
@@ -28,28 +42,30 @@ class Scene(
         changedCallbacks.remove(action)
     }
 
-    override fun indexOf(element: LayerSettings): Int = when (element) {
-        is LayerSettings.PlaneLayerSettings -> planeLayerSettingsStorage.indexOf(element)
-        else -> {
-            if (layerSettingsStorage.contains(element)) {
-                planeLayerSettingsStorage.size + layerSettingsStorage.indexOf(element)
-            } else {
-                -1
-            }
+    override fun indexOf(element: LayerSettings): Int {
+        if (element.usesCanvas) {
+            return canvasLayersStorage.indexOf(element)
         }
+
+        if (nonCanvasLayersStorage.contains(element)) {
+            return canvasLayersStorage.size + nonCanvasLayersStorage.indexOf(element)
+        }
+        return -1
     }
 
+    /**
+     * Changes index of layer in the storage.
+     * If index is invalid IndexOutOfBoundsException is thrown.
+     */
     fun changeLayerIndex(element: LayerSettings, index: Int) {
         when (element) {
-            is LayerSettings.PlaneLayerSettings -> planeLayerSettingsStorage.changeLayerIndex(element, index)
-            else -> layerSettingsStorage.changeLayerIndex(element, index)
+            is LayerSettings.PlaneLayerSettings -> canvasLayersStorage.changeLayerIndex(element, index)
+            else -> nonCanvasLayersStorage.changeLayerIndex(element, index)
         }
         changedCallbacks.forEach { it() }
     }
 
-    fun getFramesLayerSettings() = frames.flatMap { it.layers.map { layer -> layer.settings } }.distinct()
-
-    private fun<T> MutableList<T>.changeLayerIndex(element: T, index: Int) {
+    private fun <T> MutableList<T>.changeLayerIndex(element: T, index: Int) {
         remove(element)
         add(index, element)
     }
