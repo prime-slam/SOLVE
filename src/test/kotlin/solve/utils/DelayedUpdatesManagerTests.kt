fc77694c06f4633111daf3f30ec6989084397a32
Alexander-Ploskin <ploskin0107@gmail.com>
2023-05-04 14:37:37 +0300

    Refactor delayed updates logic and cover it with tests

---
+++ src/test/kotlin/solve/utils/DelayedUpdatesManagerTests.kt
@@ -0,0 +1,52 @@
+package solve.utils
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class DelayedUpdatesManagerTests {
+    private class TestUpdatable(private val updateAction: (Int) -> Unit): Updatable<Int> {
+        override fun update(data: Int) {
+            updateAction(data)
+        }
+    }
+
+    @Test
+    fun `Doesn't update when was locked`() {
+        val updatesManager = DelayedUpdatesManager<TestUpdatable, Int>()
+        var lastUsedUpdateParameter: Int? = null
+        val updatable = TestUpdatable { param -> lastUsedUpdateParameter = param }
+        updatesManager.doLockedAction {
+            updatesManager.delayUpdate(updatable, 1)
+            updatesManager.delayUpdate(updatable, 2)
+            assertNull(lastUsedUpdateParameter)
+        }
+    }
+
+    @Test
+    fun `Performs updates when unlocked`() {
+        val updatesManager = DelayedUpdatesManager<TestUpdatable, Int>()
+        val usedUpdateParams = mutableListOf<Int>()
+        val updatable1 = TestUpdatable { param -> usedUpdateParams.add(param) }
+        val updatable2 = TestUpdatable { param -> usedUpdateParams.add(param) }
+        updatesManager.doLockedAction {
+            updatesManager.delayUpdate(updatable1, 1)
+            updatesManager.delayUpdate(updatable2, 2)
+        }
+        assertEquals(2, usedUpdateParams.size)
+        assertTrue(usedUpdateParams.contains(1))
+        assertTrue(usedUpdateParams.contains(2))
+    }
+
+    @Test
+    fun `Performs only the last update for the element`() {
+        val updatesManager = DelayedUpdatesManager<TestUpdatable, Int>()
+        val usedUpdateParams = mutableListOf<Int>()
+        val updatable = TestUpdatable { param -> usedUpdateParams.add(param) }
+        updatesManager.doLockedAction {
+            updatesManager.delayUpdate(updatable, 1)
+            updatesManager.delayUpdate(updatable, 2)
+        }
+        assertEquals(1, usedUpdateParams.size)
+        assertEquals(2, usedUpdateParams[0])
+    }
+}
