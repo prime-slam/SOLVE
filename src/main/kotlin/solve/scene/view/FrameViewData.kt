878049d9cd424fb396f104269f24ea24505891c5
Alexander-Ploskin <ploskin0107@gmail.com>
2023-05-09 01:41:59 +0300

    FrameView tests

--- src/main/kotlin/solve/scene/view/FrameViewData.kt
+++ src/main/kotlin/solve/scene/view/FrameViewData.kt
@@ -2,4 +2,7 @@ package solve.scene.view
 
 import solve.scene.model.VisualizationFrame
 
+/**
+ * Represents all data that change to one FrameView, including cache reusing and virtualization.
+ */
 data class FrameViewData(val frame: VisualizationFrame?, val frameViewParameters: FrameViewParameters)
