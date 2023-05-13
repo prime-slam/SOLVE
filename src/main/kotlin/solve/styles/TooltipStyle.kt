94a853b7f5eaed8d229559f207cefc7a74145666
Alexander-Ploskin <ploskin0107@gmail.com>
2023-05-13 14:24:16 +0300

    Tooltip font style

--- src/main/kotlin/solve/styles/TooltipStyle.kt
+++ src/main/kotlin/solve/styles/TooltipStyle.kt
@@ -1,13 +1,11 @@
 package solve.styles
 
-import javafx.scene.text.FontPosture
 import tornadofx.Stylesheet
 import tornadofx.c
 
 class TooltipStyle : Stylesheet() {
     init {
         Companion.tooltip {
-            fontStyle = FontPosture.REGULAR
             fontFamily = Style.fontCondensed
             fontSize = Style.tooltipFontSize
             backgroundColor += c(Style.surfaceColor)
