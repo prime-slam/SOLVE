d0fe77c04e38b128653ec563fd3bacfd39665962
Stanislav Mishchenko <mi.sts@yandex.ru>
2023-05-14 18:23:45 +0300

    Codestyle

--- src/main/kotlin/solve/filters/settings/model/IndicesStepFilterSetting.kt
+++ src/main/kotlin/solve/filters/settings/model/IndicesStepFilterSetting.kt
@@ -3,7 +3,9 @@ package solve.filters.settings.model
 import solve.project.model.ProjectFrame
 
 class IndicesStepFilterSetting(step: Int) : FilterSetting<Int>(step) {
-    override fun apply(fields: List<ProjectFrame>) = fields.slice((settingValue - 1)..fields.lastIndex step this.settingValue)
+    override fun apply(fields: List<ProjectFrame>) = fields.slice(
+        (settingValue - 1)..fields.lastIndex step this.settingValue
+    )
 
     override fun edit(newValue: Int) {
         settingValue = newValue
