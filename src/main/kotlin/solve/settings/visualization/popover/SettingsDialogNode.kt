afdb8a1d4d149b03e379181ec3237328e4b7a10b
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-05-21 02:31:11 +0300

    added design for settings dialog

---
+++ src/main/kotlin/solve/settings/visualization/popover/SettingsDialogNode.kt
@@ -0,0 +1,19 @@
+package solve.settings.visualization.popover
+
+import javafx.scene.layout.VBox
+import solve.styles.Style
+import tornadofx.*
+
+class SettingsDialogNode : VBox(10.0) {
+
+    var title: String = ""
+
+    fun addTitle() {
+        val titleLabel =  label(title){
+            paddingLeft = 10.0
+            style = "-fx-font-family: ${Style.FontCondensed}; -fx-font-size: 28px;"
+        }
+
+        this.add(titleLabel)
+    }
+}
---
+++ src/main/kotlin/solve/styles/SettingsDialogStylesheet.kt
@@ -0,0 +1,82 @@
+package solve.styles
+
+import javafx.scene.paint.Color
+import javafx.scene.paint.Paint
+import tornadofx.*
+
+class SettingsDialogStylesheet : Stylesheet() {
+
+    companion object {
+        private val mfxSlider by cssclass()
+        private val thumbContainer by cssclass("thumb-container")
+
+        val mfxColor by cssproperty<MultiValue<Paint>>("-mfx-color")
+        val mfxRippleGenerator by cssclass("mfx-ripple-generator")
+        val mfxRippleColor by cssproperty<MultiValue<Paint>>("-mfx-ripple-color")
+        val mfxCheckBox by cssclass("mfx-checkbox")
+        val rippleContainer by cssclass("ripple-container")
+
+
+        val backgroundColour: Color = Color.valueOf(Style.BackgroundColor)
+        val primaryColor: Color = Color.valueOf(Style.PrimaryColor)
+    }
+
+    init {
+        colorPicker {
+            backgroundColor += backgroundColour
+            borderColor += box(Color.TRANSPARENT)
+            arrowButton {
+                arrow {
+                    backgroundColor += Color.valueOf(Style.PrimaryColor)
+                }
+            }
+        }
+
+        mfxCheckBox {
+            rippleContainer {
+                mfxRippleGenerator {
+                    mfxRippleColor.value += CatalogueViewStylesheet.secondaryColor
+                }
+            }
+            and(selected) {
+                box {
+                    backgroundColor += CatalogueViewStylesheet.secondaryColor
+                    borderColor += box(CatalogueViewStylesheet.secondaryColor)
+                }
+            }
+            box {
+                borderColor += box(CatalogueViewStylesheet.primaryColor)
+            }
+        }
+
+        mfxSlider {
+            bar {
+                mfxRippleGenerator {
+                    mfxColor.value += primaryColor
+                    mfxRippleColor.value += primaryColor
+                }
+                fill = Color.valueOf(Style.PrimaryColor)
+            }
+            track {
+
+                mfxColor.value = backgroundColor
+                fill = backgroundColour
+
+            }
+
+
+            thumbContainer {
+                mfxRippleGenerator {
+                    mfxRippleColor.value += primaryColor
+                }
+
+                thumb {
+                    mfxColor.value += primaryColor
+                    mfxRippleGenerator {
+                        mfxRippleColor.value += primaryColor
+                    }
+                }
+            }
+        }
+    }
+}
