faeeb799524e5b412c3e70f62139ad54a8a92762
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2023-05-21 02:14:28 +0300

    added controller for settings dialog

---
+++ src/main/kotlin/solve/settings/visualization/popover/DialogClosingController.kt
@@ -0,0 +1,8 @@
+package solve.settings.visualization.popover
+
+import javafx.beans.property.SimpleBooleanProperty
+import tornadofx.*
+
+class DialogClosingController : Controller() {
+    val isClosing = SimpleBooleanProperty(false)
+}
