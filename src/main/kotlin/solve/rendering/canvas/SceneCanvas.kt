37a9fac954a915c72e09d40144ba818096f4fcf5
Stanislav Mishchenko <mi.sts@yandex.ru>
2023-11-24 20:34:54 +0300

    don't reinit buffers for frames selections

--- src/main/kotlin/solve/rendering/canvas/SceneCanvas.kt
+++ src/main/kotlin/solve/rendering/canvas/SceneCanvas.kt
@@ -1,15 +1,27 @@
 package solve.rendering.canvas
 
+import javafx.beans.InvalidationListener
 import org.joml.Vector2f
 import solve.rendering.engine.rendering.renderers.FramesRenderer
 import solve.rendering.engine.scene.Scene
+import solve.scene.SceneFacade
 import solve.scene.controller.SceneController
 import solve.utils.ServiceLocator
+import tornadofx.*
 
 class SceneCanvas : OpenGLCanvas() {
     private var sceneController: SceneController? = null
     private var framesRenderer: FramesRenderer? = null
     private var canvasScene: Scene? = null
+
+    private val projectChangedEventHandler = InvalidationListener {
+        val controller = sceneController ?: return@InvalidationListener
+        framesRenderer?.setNewSceneFrames(controller.scene.frames)
+    }
+    private val framesChangedEventHandler = InvalidationListener {
+        val controller = sceneController ?: return@InvalidationListener
+        framesRenderer?.setFramesSelection(controller.scene.frames)
+    }
 
     init {
         initializeCanvasEvents()
@@ -20,17 +32,34 @@ class SceneCanvas : OpenGLCanvas() {
 
         val controller = ServiceLocator.getService<SceneController>() ?: return
         sceneController = controller
-        val renderer = FramesRenderer(window, controller)
+
+        val renderer = FramesRenderer(window)
         framesRenderer = renderer
         canvasScene = Scene(listOf(renderer))
+
+        addBindings()
     }
 
     override fun onDraw(deltaTime: Float) {
         canvasScene?.renderers?.forEach { it.render() }
     }
 
+    override fun onDispose() {
+        removeBindings()
+    }
+
     private fun moveCamerToLeftUpperCorner() {
         window.camera.position =
             Vector2f(window.width.toFloat(), window.height.toFloat()) / (2f * window.camera.scaledZoom)
+    }
+
+    private fun addBindings() {
+        SceneFacade.lastVisualizationKeepSettingsProperty.addListener(projectChangedEventHandler)
+        sceneController?.sceneProperty?.addListener(framesChangedEventHandler)
+    }
+
+    private fun removeBindings() {
+        SceneFacade.lastVisualizationKeepSettingsProperty.removeListener(projectChangedEventHandler)
+        sceneController?.sceneProperty?.removeListener(framesChangedEventHandler)
     }
 }
