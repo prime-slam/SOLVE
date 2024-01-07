a370198df643c22420afe2c075ab1c6f835f52c1
Stanislav Mishchenko <mi.sts@yandex.ru>
2024-01-08 00:27:30 +0300

    update opengl context fun

--- src/test/kotlin/solve/unit/rendering/utils/RenderingTestsOpenGLUtils.kt
+++ src/test/kotlin/solve/unit/rendering/utils/RenderingTestsOpenGLUtils.kt
@@ -4,7 +4,7 @@ import com.huskerdev.openglfx.lwjgl.LWJGLExecutor
 import org.lwjgl.opengl.GL.createCapabilities
 import com.huskerdev.openglfx.OpenGLCanvas as OpenGLFXCanvas
 
-internal inline fun runInOpenGLContext(crossinline action: () -> Unit) {
+internal fun runInOpenGLContext(action: () -> Unit) {
     val openGLCanvas = OpenGLFXCanvas.create(LWJGLExecutor.LWJGL_MODULE)
     openGLCanvas.addOnInitEvent {
         createCapabilities()
