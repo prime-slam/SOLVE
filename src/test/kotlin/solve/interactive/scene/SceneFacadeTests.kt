8613111bd06c24b8fc644ef5809711f4ac9ccd1f
Stanislav Mishchenko <mi.sts@yandex.ru>
2024-04-19 21:16:48 +0300

    remove check depending on app pulses

--- src/test/kotlin/solve/interactive/scene/SceneFacadeTests.kt
+++ src/test/kotlin/solve/interactive/scene/SceneFacadeTests.kt
@@ -75,23 +75,6 @@ internal class SceneFacadeTests : SceneTestsBase() {
     }
 
     @Test
-    fun `Visualize same layer twice`() {
-        val layerName = "layer1"
-        val data1 = createScene(listOf(layerName))
-        SceneFacade.visualize(data1.layers, data1.frames, false)
-        val settings = controller.scene.layerSettings.single()
-        val state = getState(0, layerName, 1)
-        state.hoverLandmark(1)
-        val data2 = createScene(listOf(layerName), framesCount = 5)
-        SceneFacade.visualize(data2.layers, data2.frames, true)
-        assertEquals(5, controller.scene.frames.size)
-        assertSame(settings, controller.scene.layerSettings.single())
-        val newState = getState(0, layerName, 1)
-        assertNotSame(state, newState)
-        assertEquals(0, newState.hoveredLandmarkUIDs.size)
-    }
-
-    @Test
     fun `Visualize different projects one after another`() {
         val layerName = "layer1"
         val data1 = createScene(listOf(layerName), "project1")
